0fc0b5a1f7e50d85fb3b9d937cf11a628357279a
anjenaex <anjenaex@gmail.com>
2021-02-20 18:18:03 +0100

    let Ruben work

--- lead-service/src/main/java/com/example/leadservice/repository/LeadRepository.java
+++ lead-service/src/main/java/com/example/leadservice/repository/LeadRepository.java
@@ -1,10 +1,14 @@
 package com.example.leadservice.repository;
 
+import com.example.leadservice.controller.dto.LeadDTO;
 import com.example.leadservice.model.Lead;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LeadRepository extends JpaRepository<Lead, Integer> {
 
-
+    //method that return a list of Leads by sales rep
+    List<LeadDTO> findBy
 
 }
---
+++ opportunities-service/src/main/java/com/ironhack/opportunitiesservice/controller/dto/AccountDTO.java
@@ -0,0 +1,75 @@
+package com.ironhack.opportunitiesservice.controller.dto;
+
+import com.ironhack.opportunitiesservice.enums.Industry;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class AccountDTO {
+
+    private int id;
+    private String industry;
+    private int employeeCount;
+    private String city;
+    private String country;
+
+    public AccountDTO() {
+    }
+
+    public AccountDTO(int id, String industry, int employeeCount, String city, String country) {
+        this.id = id;
+        this.industry = industry;
+        this.employeeCount = employeeCount;
+        this.city = city;
+        this.country = country;
+    }
+
+    public AccountDTO(String industry, int employeeCount, String city, String country) {
+        this.industry = industry;
+        this.employeeCount = employeeCount;
+        this.city = city;
+        this.country = country;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public int getEmployeeCount() {
+        return employeeCount;
+    }
+
+    public void setEmployeeCount(int employeeCount) {
+        this.employeeCount = employeeCount;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
