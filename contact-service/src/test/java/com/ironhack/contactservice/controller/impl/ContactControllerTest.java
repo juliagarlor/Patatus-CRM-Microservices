2db9403a21b8628390075ecb8464b5eea70ca78f
Aliany Crespo <alianycres@gmail.com>
2021-02-20 17:34:48 +0100

    Test Contact Finished

---
+++ contact-service/src/test/java/com/ironhack/contactservice/controller/impl/ContactControllerTest.java
@@ -0,0 +1,107 @@
+package com.ironhack.contactservice.controller.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ironhack.contactservice.model.Contact;
+import com.ironhack.contactservice.repository.ContactRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class ContactControllerTest {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+
+        List<Contact> contactList = Arrays.asList(
+                new Contact(
+                        "Juan Alberto",
+                        "665734987",
+                        "jherrador@gmail.com",
+                        "médicos con fronteras",
+                        1L
+                ),
+                new Contact(
+                        "Nacho",
+                        "647123123",
+                        "nachuatadawi@hotmail.com",
+                        "magic&phoenix",
+                        2L
+                ),
+                new Contact(
+                        "Brian",
+                        "666666666",
+                        "brianElJudio@hotmail.com",
+                        "oro and Samir friend",
+                        3L
+                )
+        );
+        contactRepository.saveAll(contactList);
+    }
+
+    @AfterEach
+    void tearDown() {
+        contactRepository.deleteAll();
+    }
+
+    @Test
+    void getContactDTOById_ParamId_Contact() throws Exception {
+        Contact contact = new Contact("Karla", "666222666", "karla@hotmail.com", "oro and Samir friend", 3L);
+        MvcResult result = mockMvc.perform(get("/contact/{id}")
+                .param("id", "1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated()).andReturn();
+        assertTrue(result.getResponse().getContentAsString().contains("Karla"));
+    }
+
+
+    @Test
+    void createContact() throws Exception {
+        Contact contact = new Contact("Karla", "666222666", "karla@hotmail.com", "oro and Samir friend", 3L);
+        String body = objectMapper.writeValueAsString(contact);
+        MvcResult result = mockMvc.perform(post("/contact/{id}").content(body)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated()).andReturn();
+
+        assertTrue(result.getResponse().getContentAsString().contains("Karla"));
+    }
+
+    @Test
+    void updateContactAccountId() throws Exception {
+        Contact contact = new Contact("Karla", "666222666", "karla@hotmail.com", "oro and Samir friend", 3L);
+        String body = objectMapper.writeValueAsString(contact);
+        mockMvc.perform(MockMvcRequestBuilders.put("/contact/{id}/account-id", 1)
+                .content(body)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Sonia"));    }
+}
---
+++ contact-service/src/test/java/com/ironhack/contactservice/service/ContactServiceTest.java
@@ -0,0 +1,88 @@
+package com.ironhack.contactservice.service;
+
+import com.ironhack.contactservice.ContactServiceApplication;
+import com.ironhack.contactservice.client.LeadClient;
+import com.ironhack.contactservice.controller.dto.LeadDTO;
+import com.ironhack.contactservice.model.Contact;
+import com.ironhack.contactservice.repository.ContactRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//@RunWi
+@SpringBootTest(classes = ContactServiceApplication.class)
+class ContactServiceTest {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @Autowired
+    private LeadClient leadClient;
+
+    @BeforeEach
+    void setUp() {
+        List<Contact> contactList = Arrays.asList(
+                new Contact(
+                        "Juan Alberto",
+                        "665734987",
+                        "jherrador@gmail.com",
+                        "médicos con fronteras",
+                        1L
+                ),
+                new Contact(
+                        "Nacho",
+                        "647123123",
+                        "nachuatadawi@hotmail.com",
+                        "magic&phoenix",
+                        2L
+                ),
+                new Contact(
+                        "Brian",
+                        "666666666",
+                        "brianElJudio@hotmail.com",
+                        "oro and Samir friend",
+                        3L
+                )
+        );
+        contactRepository.saveAll(contactList);
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        contactRepository.deleteAll();
+    }
+
+    @Test
+    void getContactById_ContactId_ContactDTO() {
+        Optional<Contact> contact = contactRepository.findByAccountId(2L);
+        assertTrue(contact.isPresent());
+        assertEquals("Nacho", contact.get().getName());
+
+    }
+
+    @Test
+    void createContact_TakingLeads_ConvertsContact() {
+        Contact contact = new Contact("Susan Honum","8765433345","susan98@gmaiil.com","Camino a casa");
+        contactRepository.save(contact);
+        assertEquals("Susan Honum", contact.getName());
+    }
+
+    @Test
+    void updateContact() {
+        Optional<Contact> contact = contactRepository.findByAccountId(2L);
+        contact.get().setEmail("prueba@gmail.com");
+        contactRepository.save(contact.get());
+        assertEquals("prueba@gmail.com", contact.get().getEmail());
+    }
+}
